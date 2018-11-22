415a967b0f35575aed015080a0a398e746eafc26
aparcerozas <aparcerozas@victoria11.danielcastelao.org>
2018-11-22 08:59:12 +0100

    SegundoCommit

--- Colaboracion/src/colaboracion/Colaboracion.java
+++ Colaboracion/src/colaboracion/Colaboracion.java
@@ -17,6 +17,7 @@ public class Colaboracion {
     public static void main(String[] args) {
         // TODO code application logic here
         //Primer commit
+        //Segundo commit
     }
     
 }
